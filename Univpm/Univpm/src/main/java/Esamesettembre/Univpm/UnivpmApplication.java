78fcac41b371b8ce0ed2b8c011b4657982d2554c
andreaalbasini <50945937+andreaalbasini@users.noreply.github.com>
2019-09-03 23:11:38 +0200

    inizio del progetto

---
+++ Univpm/Univpm/src/main/java/Esamesettembre/Univpm/UnivpmApplication.java
@@ -0,0 +1,13 @@
+package Esamesettembre.Univpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivpmApplication.class, args);
+	}
+
+}
