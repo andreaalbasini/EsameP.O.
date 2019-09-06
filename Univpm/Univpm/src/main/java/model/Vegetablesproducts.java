9a8a4b85142c2b5793b7f2f66a1acd27020b6601
andreaalbasini <50945937+andreaalbasini@users.noreply.github.com>
2019-09-06 18:35:56 +0200

    aggiunto il model
    aggiunto il modello del dataset

---
+++ Univpm/Univpm/src/main/java/model/Vegetablesproducts.java
@@ -0,0 +1,71 @@
+package model;
+
+import java.io.Serializable;
+
+
+
+public class Vegetablesproducts implements Serializable { 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String Category, SectorCode, ProductCode, ProductDesc, ProductBriefDescription, Unit, Country;
+	private int Period;
+	private double  MPMarketPrice; 
+
+	
+	public  Vegetablesproducts (String Category, String SectorCode, String ProductCode, String ProductDesc, String ProductBriefDescription,String  Unit,String  Country,  int Period, double  MPMarketPrice )
+	{
+		this.Category= Category;
+		this.SectorCode= SectorCode;
+		this.ProductCode= ProductCode;
+		this.ProductDesc= ProductDesc;
+		this.ProductBriefDescription= ProductBriefDescription;
+		this.Unit=Unit;
+		this.Country= Country;
+		this.Period= Period;
+		this.MPMarketPrice= MPMarketPrice;
+	}
+	
+	
+	public String getCategory() {
+        return Category;
+        
+    }public String getSectorCode() {
+        return SectorCode;
+        
+    }public String getProductCode() {
+        return ProductCode;
+        
+    }public String getProductDesc() {
+        return ProductDesc;
+        
+    }public String getProductBriefDescription() {
+        return ProductBriefDescription;
+        
+    }public String getUnit() {
+        return Unit;
+        
+    }public String getCountry() {
+        return Country;
+        
+    }public int getPeriod() {
+        return Period;
+        
+    }public double getMPMarketPrice() {
+        return MPMarketPrice;
+    }
+    
+    
+    public String toString() {
+        return "Products: {" +
+                "Category='" + Category  +
+                ", SectorCode='" + SectorCode + 
+                ", ProductCode='" + ProductCode + 
+                ", ProductDesc='" + ProductDesc + 
+                ", ProductBriefDescription='" + ProductBriefDescription + 
+                ", Unit=" + Unit +", Country=" + Country +", Period=" + Period +
+                ", MPMarketPrice=" + MPMarketPrice +  
+                '}';
+    }
+}
