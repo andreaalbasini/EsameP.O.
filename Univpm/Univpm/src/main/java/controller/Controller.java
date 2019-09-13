d513163241f61aa5bda2b9b3cd7fab3593cb41b1
andreaalbasini <50945937+andreaalbasini@users.noreply.github.com>
2019-09-13 18:05:23 +0200

    controllo dei metadata

--- Univpm/Univpm/src/main/java/controller/Controller.java
+++ Univpm/Univpm/src/main/java/controller/Controller.java
@@ -16,13 +16,36 @@ public class Controller {
 	private VegetablesService service;
 
 
-
+	/**
+     * Costruttore che con l'annotazione @Autowired viene lanciato automaticamente all'avvio da Spring e esegue il collegamento al Service
+     *
+     * @param service riferimento all'istanza del service inizializzata da Spring
+     */
 public Controller(VegetablesService service) {
     this.service = service;
 }
+//metodi per la comunicazione con il client che gestiscono le richieste GET e POST
 
+/**
+ * Metodo che gestisce la richiesta GET alla rotta "/data", restituisce l'intero dataset
+ *
+ * @return lista di tutti gli oggetti del dataset
+ */
+//esempio: localhost:8080/data
 	@GetMapping("/data")
     public List getAllData() {
         return service.getData();
     }
+	
+    /**
+     * Metodo che gestisce la richiesta GET alla rotta "/metadata", restituisce i metadata
+     *
+     * @return lista dei metadata
+     */
+    @GetMapping("/metadata")
+    public List<Map> getMetadata() {
+        return service.getMetadata();
+    }
+	
+	
 }
