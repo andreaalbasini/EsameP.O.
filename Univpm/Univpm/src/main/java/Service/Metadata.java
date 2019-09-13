1ca063170a7d691e331664d41f16bed8978b9636
andreaalbasini <50945937+andreaalbasini@users.noreply.github.com>
2019-09-13 18:04:48 +0200

    modifiche sui metadata

--- Univpm/Univpm/src/main/java/Service/Metadata.java
+++ Univpm/Univpm/src/main/java/Service/Metadata.java
@@ -1,5 +1,58 @@
 package Service;
 
+
+import model.Vegetablesproducts;
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+
+
+
+/**
+ * Classe utile per la gestione dei matadati quando vengono richiesti
+ */
 public class Metadata {
 
-}
+	
+
+	
+		
+		
+		private List<Map> metadata = new ArrayList<>();
+		
+		public Metadata() {
+			
+			Field[] fields = Vegetablesproducts.class.getDeclaredFields();//estrae gli attributi della classe Vegetablesproducts
+
+	        for (Field f : fields) {
+	            Map<String, String> map = new HashMap<>();
+	            //andiamo ad inserire le coppie chiave/valore
+	            map.put("alias", f.getName());
+	            map.put("sourceField", f.getName().toUpperCase());//nome del campo in csv
+	            map.put("type", f.getType().getSimpleName());
+	            metadata.add(map);
+	        	}
+
+	        }
+	        
+		/**
+		 * Metodo che ritorna la lista di mappe contenente i metadati
+		 * 
+		 * @return lista dei metadati
+		 */
+		
+		public List<Map> getMetadata() {
+			return metadata;
+			}
+
+		}
+	
+	
+	
+	
+
