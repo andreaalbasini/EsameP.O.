65d4835bc47ce24f8949f63194a6fd654fa0b807
andreaalbasini <50945937+andreaalbasini@users.noreply.github.com>
2019-09-14 12:33:41 +0200

    aggiunti metodi per media,min e max

--- Univpm/Univpm/src/main/java/Service/Statistiche.java
+++ Univpm/Univpm/src/main/java/Service/Statistiche.java
@@ -28,7 +28,46 @@ public class Statistiche {
 		return somma;
 	}
 	
-
+	
+	/**
+	 * Metodo che restituisce la media dei valori di una lista numerica
+	 * 
+	 * @param lista contiene i valori utilizzati per calcolare la media
+	 * @return media dei valori
+	 */
+	
+	public static double avg(List<Number> lista) {
+		return sum(lista)/count(lista);  //restituisce la media
+	}
+	
+	/**
+	 * Metodo che restituisce il massimo tra il valori di una lista numerica
+	 * 
+	 * @param lista contiene i valori dai quali si estrae il massimo
+	 * @return massimo dei valori
+	 */
+	public static double max(List<Number> lista) {
+		double max = lista.get(0).doubleValue();  //assegna il primo valore della lista a max
+		for(Number numero : lista) {
+			if(numero.doubleValue() > max) max = numero.doubleValue();  //scorre la lista e verifica se il valore dell'indice corrispondente � maggiore di max
+		}
+		return max;
+	}
+	
+	/**
+	 * Metodo che restituisce il minimo tra il valori di una lista numerica
+	 * 
+	 * @param lista contiene i valori dai quali si estrae il minimo
+	 * @return minimo dei valori
+	 */
+	public static double min(List<Number> lista) {
+		double min = lista.get(0).doubleValue();  //assegna il primo valore della lista a min
+		for(Number numero : lista) {
+			if(numero.doubleValue() < min) min = numero.doubleValue();  //scorre la lista e verifica se il valore dell'indice corrispondente � minore di min
+		}
+		return min;
+	}
+	
 	
 }
 
