3092779920ea28e825bce6b0d137bc7eaaede593
andreaalbasini <50945937+andreaalbasini@users.noreply.github.com>
2019-09-14 12:26:46 +0200

    Implementati alcuni metodi

--- Univpm/Univpm/src/main/java/Service/Statistiche.java
+++ Univpm/Univpm/src/main/java/Service/Statistiche.java
@@ -1,8 +1,35 @@
 package Service;
 
-
+import java.util.*;
 
 
 public class Statistiche {
+	
+	/**
+	 * Metodo che effettua un conteggio sul numero di elementi di una lista
+	 * 
+	 * @param lista contiene i valori da contare
+	 * @return dimensione della lista
+	 */
+	public static int count(List lista) {
+		return lista.size();
+	}
+	
+	/**
+	 * Metodo che restituisce la somma dei valori di una lista numerica
+	 * 
+	 * @param lista contiene i valori che andranno sommati
+	 * @return somma dei valori
+	 */
+	public static double sum(List<Number> lista) {
+		double somma=0;
+		for(Number numero : lista)
+			somma += numero.doubleValue();   //scorre la lista e somma tutti i valori
+		return somma;
+	}
+	
 
+	
 }
+
+
